f1c2d481ab208ba5f9c215ab696f8a1d722bbf6b
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-02-28 22:27:32 +0600

    find if a product is already selected then the state if not then add property quantity with value 1

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -5,11 +5,18 @@ const initialState = {
 };
 
 const productReducer = (state = initialState, action) => {
+  const selectedProduct = state.cart.find(
+    (product) => product._id === action.payload._id
+  );
+
   switch (action.type) {
     case ADD_TO_CART:
+      if (selectedProduct) {
+        return state;
+      }
       return {
         ...state,
-        cart: [...state.cart, action.payload],
+        cart: [...state.cart, { ...action.payload, quantity: 1 }],
       };
     case REMOVE_FROM_CART:
       return {
