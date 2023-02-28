ae8d1919405599408ade7311e9084efe05b6554a
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-02-28 19:33:11 +0600

    initial setup of react redux done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import ProductCard from "../components/ProductCard";
 
 const Home = () => {
@@ -11,6 +12,10 @@ const Home = () => {
   }, []);
 
   console.log(products);
+
+  const state = useSelector((state) => state);
+
+  console.log(state);
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
